85a55650a0387c81cd0881ea844ca115e822ec06
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2022-09-29 09:40:42 -0700

    Updates like routes to return the user document instead of the restaurant document

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -40,7 +40,7 @@ router.get('/:id', requireToken, async (req, res, next) => {
 router.get('/username/:username', requireToken, async (req, res, next) => {
     try {
         const user = await User.findOne({ username: req.params.username })
-        .populate('likedrestaurants')
+        // .populate('likedrestaurants')
         .populate('friends')
         .populate('messages')
         .populate('events.restaurant')
@@ -155,9 +155,9 @@ router.delete('/:userId/friends/:friendId', requireToken, (req, res, next) => {
 // Create Liked Restaurant
 // POST /users/:userId/likedrestaurants/:restaurantId
 router.post('/:userId/likedrestaurants/:restaurantId', requireToken, (req, res, next) => {
-    User.findByIdAndUpdate(req.params.userId, { $push: { likedrestaurants: req.params.restaurantId }})
-        .then(() => Restaurant.findByIdAndUpdate(req.params.restaurantId, { $push: {userLikes: req.params.userId}}, { new: true } ))
-        .then(restaurant => res.json(restaurant))
+    User.findByIdAndUpdate(req.params.userId, { $push: { likedrestaurants: req.params.restaurantId }}, { new: true })
+        .then(user => res.json(user))
+        .then(() => Restaurant.findByIdAndUpdate(req.params.restaurantId, { $push: {userLikes: req.params.userId}} ))
         .catch(next)
     console.log('liked restaurant added')
 })
@@ -165,9 +165,9 @@ router.post('/:userId/likedrestaurants/:restaurantId', requireToken, (req, res, 
 // Delete Liked Restaurant
 // DELETE /users/:userId/likedrestaurants/:restaurantId
 router.delete('/:userId/likedrestaurants/:restaurantId', requireToken, (req, res, next) => {
-    User.findByIdAndUpdate(req.params.userId, { $pullAll: { likedrestaurants: [req.params.restaurantId] }})
-        .then(() => Restaurant.findByIdAndUpdate(req.params.restaurantId, { $pullAll: { userLikes: [req.params.userId] }}, { new: true } ))
-        .then(restaurant => res.json(restaurant))
+    User.findByIdAndUpdate(req.params.userId, { $pullAll: { likedrestaurants: [req.params.restaurantId] }}, { new: true } )
+        .then(user => res.json(user))
+        .then(() => Restaurant.findByIdAndUpdate(req.params.restaurantId, { $pullAll: { userLikes: [req.params.userId] }} ))
         .catch(next)
     console.log('deleted liked restaurant')
 })
